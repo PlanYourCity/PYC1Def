be6d234e3c793888a5b445d356b92706843f4613
Adrian Blanco <adrian.blanco@innovation4security.com>
2015-07-17 10:29:49 +0200

    AJAX para boton sigueme, cambio de estilo

--- static/js/sigueme.js
+++ static/js/sigueme.js
@@ -2,28 +2,44 @@ $('document').ready(function(){
 
 	$('#follow_me').click(function(e){
 
-	var titulo = $('#activity_title').text();
-	
-	var url = "/detalle/"+titulo+"/";
-	var datos = $('#formDetalle').serialize();
+		var titulo = $('#activity_title').text();
+		
+		var url = "/detalle/"+titulo+"/";
+		var datos = $('#formDetalle').serialize();
+		var textoBoton = $(this).text();
 
-	$.ajax({
-	  method: "POST",
-	  url: url,
-	  data: datos,
-	  success: function(data){
-	  	if(data.message == true){
-	  		alertify.set('notifier','position','top-right');
-			alertify.success('¡¡Siguiendo evento!!');
-	  	}
-	  	else{
-	  		alertify.set('notifier','position','top-right');
-			alertify.error('Se ha producido un error');
-	  	}
-	  }
-	});
+		if(textoBoton === "¡Sígueme!"){
+			var accion = "follow";
+		}else{
+			var accion = "unfollow";
+		}
 
-	return false;
+		$.ajax({
+		  method: "POST",
+		  url: url,
+		  data: datos+"&action="+accion,
+		  success: function(data){
+		  	if(data.message == true){
+		  		if(textoBoton === "¡Sígueme!"){
+					$("#follow_me").animate({
+						backgroundColor: "red"
+					},500);
+					$("#follow_me").text("Dejar de seguir");
+				}else{
+					$("#follow_me").animate({
+						backgroundColor: "#2E6EA5"
+					},500);
+					$("#follow_me").text("¡Sígueme!");
+				}
+		  	}
+		  	else{
+		  		alertify.set('notifier','position','top-right');
+				alertify.error('Se ha producido un error');
+		  	}
+		  }
+		});
+
+		return false;
 
 	});
 	
