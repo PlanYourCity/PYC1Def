85b27efbda979a543c1d0618ef6e903331e72312
Adrian Blanco <adrian.blanco@innovation4security.com>
2015-07-22 14:33:30 +0200

    datepicker de ofertar modificado

--- static/js/ofertar.js
+++ static/js/ofertar.js
@@ -1,5 +1,12 @@
-$(function() {
-    $( ".datepicker" ).datepicker();
+$('document').ready(function() {
+
+    /*DATEPICKER PARA LA FECHA*/
+    $( ".calendar" ).datepicker({
+        showOn: "button",
+        buttonImage: "/static/images/iconos/calendar.svg",
+        altField: "input[name='Fecha']"
+    });
+
     $.datepicker.regional['es'] = {
         closeText: 'Cerrar',
         prevText: '<Ant',
@@ -18,5 +25,6 @@ $(function() {
         yearSuffix: '',
         minDate: 0
     };
-    $.datepicker.setDefaults($.datepicker.regional['es']);
+    $.datepicker.setDefaults($.datepicker.regional['es']);    
+
 });
