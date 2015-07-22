920b30139e6734bdfb2aafcded39669a4d8ea2f5
Youssef Banay El Youssfi <youssefbannay@gmail.com>
2015-07-22 11:26:24 +0200

    calendario

--- PYC/settings.py
+++ PYC/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'fullcalendar',
+    #'fullcalendar',
     'social.apps.django_app.default',
 )
 
